9ea75007540e2dd85824ac7ec277c8d801a1d7b0
Klaas Van Parys <klaas@ivox.be>
2015-10-14 17:58:46 +0200

    Composer script test

---
+++ src/PlinthScripts/ComposerHandler.php
@@ -0,0 +1,21 @@
+<?php
+namespace PlinthScripts;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class ComposerHandler {
+	
+	public static function createProject(Event $event) {
+	
+		print __DIR__;
+	
+	}
+	
+	public static function preInstall() {
+		
+		
+		
+	}
+	
+}
