26e0c31159817e4ee6e0ac156b892b271701840d
Klaas Van Parys <klaas@ivox.be>
2017-08-11 10:51:34 +0200

    Added on duplicate key update to the insert query

--- src/Plinth/Database/Query/InsertQuery.php
+++ src/Plinth/Database/Query/InsertQuery.php
@@ -3,8 +3,9 @@
 namespace Plinth\Database\Query;
 
 use Plinth\Exception\PlinthException;
-class InsertQuery extends BaseQuery {
-	
+
+class InsertQuery extends BaseQuery
+{
 	const VALUES_COLUMNS = 0;
 	const VALUES_ONLY = 1;
 	const SELECT_ONLY = 2;
@@ -26,18 +27,23 @@ class InsertQuery extends BaseQuery {
 	private $_values = array();
 
 	/**
+	 * Column => Data for ON DUPLICATE KEY UPDATE
+	 *
+	 * @var array
+	 */
+	private $_update = array();
+
+	/**
 	 * @var SelectQuery
 	 */
 	private $_selectQuery;
+
 	/**
-	 * On first insert set InsertQuery type
-	 * Force a user to use only the values in his query, a value-column combination or setColumn with multiple values
-	 * 
-	 * @param string $useColumn
+	 * @param integer $useType
 	 * @throws PlinthException
 	 */
-	private function setType ($useType) {
-		
+	private function setType ($useType)
+	{
 		if ($this->_type === NULL) {
 		    $this->_type = $useType !== self::VALUES_ONLY && $useType !== self::SELECT_ONLY && $useType !== self::VALUES_MULTIPLE ? self::VALUES_COLUMNS : $useType;
 		} else {
@@ -47,53 +53,46 @@ class InsertQuery extends BaseQuery {
 			&&	($this->_type === self::VALUES_ONLY && $useType !== false))
 			throw new PlinthException('Please only use values or a value-column combination for all your inserts.');
 		}
-		
 	}
-	
+
 	/**
-	 * @param multitype $value
-	 * @param string $column
+	 * @param $value
+	 * @param int $column
+	 * @return $this
+	 * @throws PlinthException
 	 */
-	public function insert ($value, $column=self::VALUES_ONLY) {
-		
+	public function insert ($value, $column=self::VALUES_ONLY)
+	{
 		if ($this->_type === self::VALUES_MULTIPLE) {
-			
 			if (!is_array($value)) throw new PlinthException('Please use an array for the column values');
-						
 		} else {
-		
 			if (is_array($value)) throw new PlinthException('Please use setColumns first before you insert multiple values');
 			
 			$this->setType($column);
 		
-			if ($this->_type === self::VALUES_COLUMNS)	$this->_columns[] = $column;		
-		
+			if ($this->_type === self::VALUES_COLUMNS)	$this->_columns[] = $column;
 		}
-		
 		$this->_values[] = $value;
 		
 		return $this;
-					
 	}
-	
+
 	/**
 	 * @param SelectQuery $query
 	 * @param array $columns
+	 * @return $this
+	 * @throws PlinthException
 	 */
-	public function insertSelectQuery(SelectQuery $query, $columns=array()) {
-	    
+	public function insertSelectQuery(SelectQuery $query, $columns=array())
+	{
 	    if ($this->_type === NULL) {
-	    
     	    $this->setType(self::SELECT_ONLY);
-    	    
+
     	    $this->_columns = $columns;
-    	    
     	    $this->_selectQuery = $query;
-	    
 	    } else throw new PlinthException('insertSelectQuery can only be use once and cannot be combined with other inserts.');
 	    
 	    return $this;
-	    
 	}
 	
 	/**
@@ -101,86 +100,108 @@ class InsertQuery extends BaseQuery {
 	 * @throws PlinthException
 	 * @return InsertQuery
 	 */
-	public function setColumns($columns=array()) {
-		
+	public function setColumns($columns=array())
+	{
 		if ($this->_type === NULL) {
-	    
     	    $this->setType(self::VALUES_MULTIPLE);
     	    
     	    $this->_columns = $columns;
-	    
 	    } else throw new PlinthException('setColumns can only be use once and cannot be combined with other inserts.');
 	    
 	    return $this;
-		
 	}
 
 	/**
 	 * @return boolean
 	 */
-	private function hasData()	{ return count($this->_values) > 0; }
+	private function hasData() { return count($this->_values) > 0; }
 	
 	/**
 	 * @throws PlinthException
 	 * @return string
 	 */
-	private function getData()	{
-
+	private function getData()
+	{
 		$data = false;
 		
 		if ($this->_type === self::VALUES_COLUMNS) {
-			
 			if (count($this->_columns) === count($this->_values)) {
-				
 				$data = " (" . implode(',', $this->_columns) . ") VALUES (" . implode(',', $this->_values) . ")";
-				
 			} else throw new PlinthException('Your columns and values must match');
-
 		} elseif ($this->_type === self::VALUES_MULTIPLE) {
-			
 			$columns = count($this->_columns);
-			
 			$data = " (" . implode(',', $this->_columns) . ") VALUES ";
 			$dataValues = array();
 			
 			if (!$this->hasData()) throw new PlinthException('Please insert values');
 						
 			foreach ($this->_values as $i => $row) {
-				
 				if (count($row) === $columns) $dataValues[] = "(" . implode(',', $row) . ")";
 				else throw new PlinthException('Your values must match the number of columns');
-				
 			}
 			
 			$data .= implode(', ', $dataValues);
-			
 		} elseif ($this->_type === self::SELECT_ONLY) {
-		
 		    $data = (count($this->_columns) > 0 ? " (" . implode(',', $this->_columns) . ") " : " ") . $this->_selectQuery->get(false);
-		    		    
 		} else {
-			
 			$data = " VALUES (" . implode(',', $this->_values) . ")";
-			
 		}
 		
-		return $data !== false ? $data : " () VALUES ()"; 
-		 	
+		return $data !== false ? $data : " () VALUES ()";
 	}
 	
 	/**
 	 * @return string
 	 */
 	private function getInsert() { return "INSERT INTO "; }
-	
+
+	/**
+	 * @return string
+	 */
+	private function getOnDuplicateKeyUpdate() { return " ON DUPLICATE KEY UPDATE "; }
+
+	/**
+	 * @param $column
+	 * @param $value
+	 * @return $this
+	 */
+	public function updateOnDuplicateKey ($column, $value)
+	{
+		$this->_update[$column] = $value;
+
+		return $this;
+	}
+
+	/**
+	 * @return bool
+	 */
+	private function hasUpdateData() { return count($this->_update) > 0; }
+
+	/**
+	 * @return string
+	 */
+	private function getUpdateData()
+	{
+		$updateData = [];
+		foreach ($this->_update as $column => $value) {
+			$updateData[] = $column . "=" . $value;
+		}
+
+		return implode(', ', $updateData);
+	}
+
 	/** 
 	 * (non-PHPdoc)
 	 * @see IQuery::get()
 	 */
-	public function get($end=true) {
+	public function get($end = true)
+	{
+		$insert = $this->getInsert() . " " . $this->getTable() . $this->getData();
 
-		return  $this->getInsert() . " " . $this->getTable() . $this->getData() . $this->getEnd($end);
+		if ($this->hasUpdateData()) {
+			$insert .= $this->getOnDuplicateKeyUpdate() . $this->getUpdateData();
+		}
 
+		return $insert . $this->getEnd($end);
 	}
-
 }
