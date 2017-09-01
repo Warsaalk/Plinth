48555905d0c29226ec4e943ed93539ebb14219db
Klaas Van Parys <klaas@ivox.be>
2017-09-01 11:04:21 +0200

    Ability to define a table to delete in the DeleteQuery
    Can be used when selecting multiple tables in a delete statement. For example when joining tables.

--- src/Plinth/Database/Query/DeleteQuery.php
+++ src/Plinth/Database/Query/DeleteQuery.php
@@ -5,29 +5,73 @@ namespace Plinth\Database\Query;
 class DeleteQuery extends WhereQuery {
 
 	/**
+	 * @var array
+	 */
+	private $tablesToDelete = array();
+
+	/**
+	 * @return boolean
+	 */
+	protected function hasTablesToDelete()
+	{
+		return count($this->tablesToDelete) > 0;
+	}
+
+	/**
+	 * Used when selecting multiple tables like joins
+	 *
+	 * @param $table
+	 * @return $this
+	 */
+	public function delete($table)
+	{
+		if (in_array($table, $this->tablesToDelete) === false) {
+			$this->tablesToDelete[] = $table;
+		}
+
+		return $this;
+	}
+
+	/**
 	 * @return string
 	 */
-	private function getDelete()	{ return "DELETE";						}
+	private function getDelete()
+	{
+		return "DELETE";
+	}
+
+	/**
+	 * @return string
+	 */
+	private function getTablesToDelete()
+	{
+		return " " . implode(', ', $this->tablesToDelete);
+	}
 	
 	/**
 	 * @return string
 	 */
-	private function getFrom()		{ return " FROM " . $this->getTable();	}
+	private function getFrom()
+	{
+		return " FROM " . $this->getTable();
+	}
 
 	/**
 	 * (non-PHPdoc)
 	 * @see IQuery::get()
 	 */
-	public function get($end=true) {
+	public function get($end=true)
+	{
+		$return = $this->getDelete();
 
-		$return = $this->getDelete() . $this->getFrom();
+		if ($this->hasTablesToDelete())	$return .= $this->getTablesToDelete();
+
+		$return .= $this->getFrom();
 			
 		if ($this->hasAs()) 	$return .= $this->getAs();
 		if ($this->hasWhere()) 	$return .= $this->getWhere();
 		if ($this->hasLimit()) 	$return .= $this->getLimit();
 			
 		return  $return . $this->getEnd($end);
-
 	}
-
 }
