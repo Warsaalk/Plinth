bdd4e3b93326bababce5d5b32c0662ba7b29b4bf
Klaas Van Parys <klaas@ivox.be>
2018-03-16 18:10:51 +0100

    Changed the deprecated mcrypt to a hashing algoritme for session storage

--- src/Plinth/User/UserService.php
+++ src/Plinth/User/UserService.php
@@ -8,8 +8,8 @@ use Plinth\Exception\PlinthException;
 use Plinth\Validation\Validator;
 use Plinth\Common\Debug;
 
-class UserService extends Connector {
-
+class UserService extends Connector
+{
 	/**
 	 * @var User
 	 */
@@ -33,46 +33,41 @@ class UserService extends Connector {
 	/**
 	 * @return User
 	 */
-	public function getUser() {
-		 
+	public function getUser()
+	{
 		return $this->user;
-	
 	}
 	
 	/**
 	 * @return boolean
 	 */
-	public function hasUser() {
-		 
+	public function hasUser()
+	{
 		return $this->user !== NULL;
-	
 	}
 	
 	/**
 	 * @param UserRepository $userrepository
 	 */
-	public function setUserRepository(UserRepository $userrepository) {
-		
+	public function setUserRepository(UserRepository $userrepository)
+	{
 		$this->userrepository = $userrepository;
-		
 	}
 	
 	/**
 	 * @return boolean
 	 */
-	public function isSessionValid() {
-		 
+	public function isSessionValid()
+	{
 		return $this->validSession;
-		 
 	}
 	
 	/**
 	 * @return boolean
 	 */
-	public function isLoggedOut() {
-		
+	public function isLoggedOut()
+	{
 		return $this->loggedOut;
-		
 	}
 
 	/**
@@ -82,39 +77,36 @@ class UserService extends Connector {
 	 * http://php.net/manual/en/function.password-hash.php
 	 *
 	 * @param $token
-	 * @return $string
+	 * @return string
 	 */
-	public function getHashForToken($token) {
-
+	public function getHashForToken($token)
+	{
 		return password_hash($token, PASSWORD_DEFAULT);
+	}
 
-	}
-	
 	/**
-	 * @param string $token
-	 * @param function $validationCallback
-	 * @return boolean
+	 * @param $token
+	 * @param callable $validationCallback
+	 * @return bool
+	 * @throws PlinthException
 	 */
-	public function loginWithToken($token, $validationCallback = NULL) {
-		
+	public function loginWithToken($token, $validationCallback = NULL)
+	{
 		return $this->login(NULL, $token, $validationCallback);
-		
 	}
-	
+
 	/**
-	 * @param string $login
-	 * @param string $token
-	 * @param function $validationCallback
-	 * @return boolean
+	 * @param $login
+	 * @param $token
+	 * @param callable $validationCallback
+	 * @return bool
+	 * @throws PlinthException
 	 */
-	public function login($login, $token, $validationCallback = NULL) {
-		    
+	public function login($login, $token, $validationCallback = NULL)
+	{
 	    $this->user = $this->checkCreds($login, $token);
-		
 		if($this->user !== NULL) {
-		
 			if ($validationCallback === NULL || $validationCallback($this->user)) {
-			
 				session_regenerate_id(true); //Always generate new id on login
 								
 				if ($this->Main()->getSetting('usersession')) {
@@ -127,94 +119,68 @@ class UserService extends Connector {
 				$_SESSION['plinth_user_generated'] = time();
 				
 				return true;
-			
 			}
-			
 		}
-		
 		return false;
-		
 	}
 
 	/**
-	 * @param string $login
-	 * @param string $token
-	 * @return User|boolean
+	 * @param $login
+	 * @param $token
+	 * @return bool|null|User
+	 * @throws PlinthException
 	 */
-	private function checkCreds($login, $token) {
-
+	private function checkCreds($login, $token)
+	{
 	    if ($login === NULL) {
-
 			$tokensalt = $this->Main()->config->get('keys:tokensalt');
-
 			if ($tokensalt !== false) {
-
 				$usertoken = crypt($token, $tokensalt);
 				$userlogin = $this->userrepository->findUserWithToken($usertoken);
 
 				if ($userlogin && $userlogin->canLogin()) {
-
 					$userlogin->clearToken();
-
 					return $userlogin;
-
 				}
-
 			} else {
-
 				throw new PlinthException('Please define a token salt in your config if you want to use the token only authentication.');
-
 			}
-	    	
 		} else {
-	    	
 	    	$userlogin = $this->userrepository->findUserWithLogin($login);
-
 			if ($userlogin && $userlogin->canLogin()) {
-			
 				if (password_verify($token, $userlogin->getToken())) {
-
 					if ($this->Main()->getSetting('userrehash') === true && password_needs_rehash($userlogin->getToken(), PASSWORD_DEFAULT)) {
-
 						$this->userrepository->updateUserToken($userlogin->getID(), $this->getHashForToken($token));
-
 					}
 
 					$userlogin->clearToken();
 
 					return $userlogin;
-					
 				}
-				
 			}
-			
 		}
-		
 		return NULL;
-		
-	}	
-    
-    /**
-     * @return boolean
-     */
-	public function verifySession(){
+	}
 
+	/**
+	 * @return bool
+	 * @throws PlinthException
+	 */
+	public function verifySession()
+	{
 		if (isset($_SESSION['plinth_user_id'])) {
-			
 			if ($this->user === NULL) {
 				$this->user = $this->userrepository->find($_SESSION['plinth_user_id']);
-			}	
-						
+			}
+
 			if ($this->user !== NULL) {
 				//If application uses usersessions
 				if ($this->Main()->getSetting('usersession')) {
-					
 					$current_session = $this->user->getSession();
 					
 					//Check if User::getSession is implemented
 					if ($current_session !== false) {
-						
-						$check_session = $this->encrypt_session();
+						$check_session = $this->hashSession();
 						
 						//Compare current session against the session generator
 						if (strcmp($current_session, $check_session) === 0) {
@@ -224,50 +190,37 @@ class UserService extends Connector {
 								$now = time();
 								if ($now > $_SESSION['plinth_user_generated'] + $this->Main()->getSetting('sessionregenerate')) {
 									session_regenerate_id(); //Don't use true as it'll delete the old session
-									$this->userrepository->updateUserSession($this->user->getID(), $this->encrypt_session());
+									$this->userrepository->updateUserSession($this->user->getID(), $this->hashSession());
 									$_SESSION['plinth_user_generated'] = $now;
 								}
 							}
 								
 							return $this->validSession = true;
-						
-						} else $this->logout();
+						} else {
+							$this->logout();
+						}
 					}				
 				} else {
 					return $this->validSession = true;
 				}			
 			}
-				
 		}
-		
 		return false;
-		
 	}
 	
-	/** 
+	/**
 	 * @return string
 	 */
-	private function encrypt_session(){
-	
-		$mcryptkey   = self::Main()->config->get('keys:mcrypt');
-		$ivsalt      = self::Main()->config->get('keys:ivsalt');
-		
-		$string      = session_id() . "." . $_SERVER['REMOTE_ADDR'] . "." . $_SERVER['HTTP_USER_AGENT'];
-				
-		$mcryptkey   = substr($mcryptkey, -24); //algorithm recuires key with length 24
-		$crypttext   = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $mcryptkey, $string, MCRYPT_MODE_CBC, $ivsalt);
-		
-		unset($mcryptkey, $ivsalt);
-		
-		return base64_encode($crypttext);
-			
+	private function hashSession()
+	{
+		return $this->getHashForToken(session_id() . "." . $_SERVER['REMOTE_ADDR'] . "." . $_SERVER['HTTP_USER_AGENT']);
 	}
 	
 	/**
 	 * @return boolean
 	 */
-	public function logout(){
-		
+	public function logout()
+	{
 		$this->user = NULL;
 		$this->loggedOut = true;
 		
@@ -282,7 +235,5 @@ class UserService extends Connector {
 		session_regenerate_id(true); 
 		
 		return true;
-		
 	}
-
 }
