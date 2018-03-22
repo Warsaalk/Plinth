c557fcf0e25fe3a10642440b7acbfe2d30b5c723
Klaas Van Parys <klaas@ivox.be>
2018-03-22 16:28:16 +0100

    Keep a repository it's state during the runtime instead of making a new instance every time

--- src/Plinth/Entity/EntityRepository.php
+++ src/Plinth/Entity/EntityRepository.php
@@ -3,19 +3,33 @@
 namespace Plinth\Entity;
 
 use Plinth\Connector;
+use Plinth\Exception\PlinthException;
 
-class EntityRepository extends Connector {
-	
+class EntityRepository extends Connector
+{
 	/**
-	 * @param string $fqcn
-	 * @return mixed
+	 * @var EntityRepository[]
 	 */
-	public function getRepository($fqcn) {
-		
-		$repositoryClass = $fqcn . 'Repository';
-		
-		return new $repositoryClass($this->Main());
-		
+	private $repositories;
+
+	/**
+	 * @param $fqcn
+	 * @return EntityRepository
+	 * @throws PlinthException
+	 */
+	public function getRepository($fqcn)
+	{
+		if (!in_array(self::class, class_parents($fqcn))) {
+			$fqcn .= 'Repository'; // Legacy check
+			if (!in_array(self::class, class_parents($fqcn))) {
+				throw new PlinthException("Your repository, $fqcn, must extend " . self::class);
+			}
+		}
+
+		if (!isset($this->repositories[$fqcn])) {
+			$this->repositories[$fqcn] = new $fqcn($this->main);
+		}
+
+		return $this->repositories[$fqcn];
 	}
-	
 }
