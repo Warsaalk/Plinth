4e7b6ba193757541ffee1c2a1c6788b06a94d606
Klaas Van Parys <klaas@ivox.be>
2019-06-06 13:49:02 +0200

    Removed old ORM references

--- src/Plinth/Entity/_ORM/relationship/manytoonerelationship.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Plinth\Entity\ORM\Relationship;
-
-class ManyToOneRelationship extends Relationship {
-	
-	protected $type = self::MANY_TO_ONE;
-	
-}
--- src/Plinth/Entity/_ORM/relationship/onetoonerelationship.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Plinth\Entity\ORM\Relationship;
-
-class OneToOneRelationship extends Relationship {
-	
-	protected $type = self::ONE_TO_ONE;
-	
-}
