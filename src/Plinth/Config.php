a5e548eb3047b75a59fd01c2b8aca9343f2aafd4
Klaas Van Parys <klaas@ivox.be>
2019-07-25 14:18:17 +0200

    Allow config references inside the config itself with $CNF & added support for inline replacements for scalar variables. This can be done by using {$ENV.key.subkey}

--- src/Plinth/Config.php
+++ src/Plinth/Config.php
@@ -8,6 +8,8 @@ class Config
 {
 	const ENV_FILE = "env.ini";
 
+	const REF_REGEX = '\$(ENV|CNF)((?:\.\w+)+)';
+
 	/**
 	 * @var array
 	 */
@@ -26,11 +28,11 @@ class Config
 	public function __construct($file)
 	{
 		if (file_exists(__BASE_ROOT . self::ENV_FILE)) {
-			$this->env = $this->parse(parse_ini_file(__BASE_ROOT . self::ENV_FILE, true, INI_SCANNER_RAW));
+			$this->parse($this->env, parse_ini_file(__BASE_ROOT . self::ENV_FILE, true, INI_SCANNER_RAW));
 		}
 
 		if (file_exists($file)) {
-			$this->config = $this->parse(parse_ini_file($file, true, INI_SCANNER_RAW), true);
+			$this->parse($this->config, parse_ini_file($file, true, INI_SCANNER_RAW), true);
 		} else {
 			throw new PlinthException("Your config file, $file, cannot be found.");
 		}
@@ -39,32 +41,60 @@ class Config
 	}
 
 	/**
-	 * @param $value
+	 * @param string $refType ENV|CONF
+	 * @param mixed $value
 	 * @return mixed
 	 */
-	private function getEnvValue ($value)
+	private function getRefValue ($refType, $value)
 	{
 		$path = explode(".", $value);
 
-		array_shift($path); // Remove the $ENV
+		array_shift($path); // Remove the leading . (dot)
 
-		return array_reduce($path, function ($result, $item) {return isset($result[$item]) ? $result[$item] : null;}, $this->env);
+		return array_reduce($path, function ($result, $item) {return isset($result[$item]) ? $result[$item] : null;}, $refType === "CNF" ? $this->config : $this->env);
 	}
 
 	/**
 	 * @param $value
-	 * @param bool $checkEnv
+	 * @param bool $checkREF
 	 * @return boolean|integer|string
+	 * @throws PlinthException
 	 */
-	private function processValue($value, $checkEnv = false)
+	private function processValue($value, $checkREF = false)
 	{
 		if (preg_match('/^true|false$/', $value)) {
 			return $value === 'true' ? true : false;
 		} elseif (preg_match('/^\d+$/', $value)) {
 			if ($value > PHP_INT_MAX) return $value;
 			return (int)$value;
-		} elseif ($checkEnv && preg_match('/^\$ENV(\.\w+)+$/', $value)) {
-			return $this->getEnvValue($value);
+		} elseif ($checkREF) {
+			// Full value match (allows all types of ref values)
+			if (preg_match('/^' . self::REF_REGEX . '$/', $value, $match) === 1) {
+				return $this->getRefValue($match[1], $match[2]);
+			}
+
+			// Replacement match (allows only scalar variables)
+			preg_match_all('/\{' . self::REF_REGEX . '\}/', $value, $matches, PREG_SET_ORDER);
+			if (count($matches) > 0) {
+				$replacements = [];
+				foreach ($matches as $match) {
+					// Build-in check to avoid the same reference to be replaced twice
+					if (!in_array($match[0], $replacements)) {
+						$replacements[] = $match[0];
+						$replacement = $this->getRefValue($match[1], $match[2]);
+
+						if (!is_scalar($replacement)) {
+							if ($replacement === null) {
+								throw new PlinthException("Config error: the result of {$match[0]} is null/empty.");
+							} else {
+								throw new PlinthException("Config error: the result of {$match[0]} is not a scalar value. You can't replace object/array values using the {\$} notation.");
+							}
+						}
+
+						$value = str_replace($match[0], $replacement, $value);
+					}
+				}
+			}
 		}
 
 		return $value;
@@ -74,47 +104,54 @@ class Config
 	 * @param $keys
 	 * @param $value
 	 * @param array $array
-	 * @param bool $checkEnv
+	 * @param bool $checkREF
 	 * @throws PlinthException
 	 */
-	private function build($keys, $value, &$array = [], $checkEnv = false)
+	private function build($keys, $value, &$array = [], $checkREF = false)
 	{
 		if (count($keys) == 1) {
-			$array[$keys[0]] = is_array($value) ?
-				$this->parse($value, $checkEnv) :
-				$this->processValue($value, $checkEnv);
+			if (is_array($value)) {
+				$this->parse($array[$keys[0]], $value, $checkREF);
+			} else {
+				$array[$keys[0]] = $this->processValue($value, $checkREF);
+			}
 		} else {
 			$key = array_shift($keys);
 
 			if(!isset($array[$key])) $array[$key] = [];
 
-			$this->build($keys, $value, $array[$key], $checkEnv);
+			$this->build($keys, $value, $array[$key], $checkREF);
 		}
 	}
 
 	/**
+	 * @param $destination
 	 * @param $config
-	 * @param bool $checkEnv
+	 * @param bool $checkREF
 	 * @return array
 	 * @throws PlinthException
 	 */
-	private function parse($config, $checkEnv = false)
+	private function parse(&$destination, $config, $checkREF = false)
 	{
 		if ($config === false) throw new PlinthException('Your config/env file contains some errors');
 
-		$parsed = [];
+		if ($destination === null) {
+			$destination = [];
+		}
 
 		foreach ($config as $key => $value) {
 			if (preg_match( '/^(?!\.).*(?<!\.)$/', $key) && preg_match('/\./', $key)) { //Contains a point but doesn't start or end with one
-				$this->build(explode('.', $key), $value, $parsed, $checkEnv);
+				$this->build(explode('.', $key), $value, $destination, $checkREF);
 			} else {
-				$parsed[$key] = is_array($value) ?
-					$this->parse($value, $checkEnv) :
-					$this->processValue($value, $checkEnv);
+				if (is_array($value)) {
+					$this->parse($destination[$key], $value, $checkREF);
+				} else {
+					$destination[$key] = $this->processValue($value, $checkREF);
+				}
 			}
 		}
 
-		return $parsed;
+		return $destination;
 	}
 
 	/**
